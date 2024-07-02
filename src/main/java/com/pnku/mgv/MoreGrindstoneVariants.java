960b2075bfbb5c324f763607031f6025ef295dc8
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-07-02 17:17:13 +0200

    Initial Commit (1.21)

--- src/main/java/com/pnku/mgv/MoreGrindstoneVariants.java
+++ src/main/java/com/pnku/mgv/MoreGrindstoneVariants.java
@@ -17,6 +17,6 @@ public class MoreGrindstoneVariants implements ModInitializer {
     }
 
     public static ResourceLocation asId(String path) {
-        return new ResourceLocation(MODID, path);
+        return ResourceLocation.fromNamespaceAndPath(MODID, path);
     }
 }
